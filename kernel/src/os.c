2897edd4bd2b4a63e965baa79e72070bbd05b4f0
tracer-nju <tracer@nju.edu.cn>
2023-03-18 01:56:07 +0800

    Linux ysy-virtual-machine 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:56:07 up 2 days, 20:42,  3 users,  load average: 0.12, 0.12, 0.07

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -113,9 +113,10 @@ static void os_run()
 	os_run_test2();
 	os_run_test3();
 #endif
-	printf("%x\n",pmm->alloc(8192));
-	printf("%x\n",pmm->alloc(8192));
+	printf("%x\n",pmm->alloc(5368));
 	printf("%x\n",pmm->alloc(4096));
+	printf("%x\n",pmm->alloc(5368));
+	printf("%x\n",pmm->alloc(4));
 
 	while(true);
 }
